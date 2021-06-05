211ae022f9f0ee1dc7cb86f6d7da682fd2744ef6
Daniel Dutton <DannyDuttster@gmail.com>
2021-06-05 12:07:54 +0100

    Misc

--- Wordmanship/Services/UKEnglishWordService.swift
+++ Wordmanship/Services/UKEnglishWordService.swift
@@ -5,7 +5,7 @@ class UKEnglishWordService: WordService {
     //
     func getRandomWord() -> AnyPublisher<Word, Never> {
         var request = URLRequest(url: URL(string: "https://wordsapiv1.p.mashape.com/words?random=true")!)
-        request.addValue("X-Mashape-Key", forHTTPHeaderField: "7b4084b0cdmsh0c4123f9c0af4b5p13c2bajsn166bdcd748e7")
+        request.addValue("X-Mashape-Key", forHTTPHeaderField: "---")
         request.addValue("Accept", forHTTPHeaderField: "application/json")
         
         return URLSession.shared.dataTaskPublisher(for: request)
