ff77281845dde8e4d3846848e21d4cc9521c06a5
Daniel Dutton <DannyDuttster@gmail.com>
2021-06-05 11:42:21 +0100

    Initial Commit

---
+++ Wordmanship/WordmanshipApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordmanshipApp.swift
+//  Wordmanship
+//
+//  Created by Daniel Dutton on 05/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct WordmanshipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
